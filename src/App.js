d96cf30efb5a9c7a0c1327b41ded482b2c5b3d11
SoheilRamezani <soheilramezani.dev@gmail.com>
2022-09-13 14:57:26 +0430

    state move to App for show image in another component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import Images from "./components/content/Images";
 import SearchForm from "./components/header/SearchForm";
+import { useState } from "react";
+
 
 const App = () => {
+
+    const [images,setImages] = useState([]);
+
     return <div>
-        <SearchForm/>
+        <SearchForm setImages={setImages} />
+        <Images images={images} />
     </div>
 }
  
