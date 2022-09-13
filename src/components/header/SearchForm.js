f3fa5185dab5e1d512f310f70659bc9235c7e163
SoheilRamezani <soheilramezani.dev@gmail.com>
2022-09-13 14:56:39 +0430

    form for search created

--- src/components/header/SearchForm.js
+++ src/components/header/SearchForm.js
@@ -1,16 +1,51 @@
-import './SearchForm.css'
+import "./SearchForm.css";
+import { useFormik } from "formik";
+import * as Yup from 'yup';
+import axios from "axios";
 
-const SearchForm = () => {
-    return <section className="search-form d-flex justify-content-center p-4">
-        <form className="form-inline" onSubmit={''}>
-            <div className="form-group">
-                <label> Search image from Unsplash : </label>
-                    <input type='text' className="form-input" placeholder="search image" onChange={''} value ="" />
-                
-                <submit className="btn btn-success ms-2" >Search</submit>
-            </div>
-        </form>
+const SearchForm = ({setImages}) => {
+
+
+  const formik = useFormik({
+    initialValues: {
+        search: "",
+    },
+    validationSchema: Yup.object({
+      search: Yup.string().min(2).required(),
+    }),
+    onSubmit: async (values) => {
+        const Access_Key = "RA-bhLdyT-UE8NUBrShCC83KnEEZjmBZjWyrVD-B62M";
+      const response = await axios.get(
+        `https://api.unsplash.com/search/photos?per_page=20&page=2&query=${values.search}&client_id=${Access_Key}`
+      );
+      setImages(response.data.results)
+    },
+  });
+
+  return (
+    <section className="search-form d-flex justify-content-center p-4">
+      <form className="form-inline" onSubmit={formik.handleSubmit}>
+        <div className="form-group">
+          <label> Search image from Unsplash : </label>
+          <input
+          id="search"
+            name="search"
+            type="text"
+            className="form-input"
+            placeholder="search image"
+            onChange={formik.handleChange}
+            value={formik.values.search}
+            onBlur={formik.hadleBlur}
+          />
+          {formik.touched.search && formik.errors.search ? (
+            <div>{formik.errors.search}</div>
+          ) : null}
+
+          <input type="submit" className="btn btn-success ms-2" value="Search" />
+        </div>
+      </form>
     </section>
-}
- 
+  );
+};
+
 export default SearchForm;
